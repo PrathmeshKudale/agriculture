3d7e5ce0ac4db31f773f111436d547d67866a9f6
PrathmeshKudale <kudaleprathmesh14@gmail.com>
2026-01-27 15:06:18 +0530

    Change text color to light grey in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ st.markdown("""
     
     /* 2. TEXT VISIBILITY: Dark Grey/Black */
     h1, h2, h3, h4, h5, h6, p, span, div, label, li, .stMarkdown {
-        color: #1f2937 !important;
+        color: #ced9eb !important;
     }
     
     /* 3. CARDS: White with Soft Shadow */
